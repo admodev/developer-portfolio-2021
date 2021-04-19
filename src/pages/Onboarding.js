2fafa90a7c5702ba717083f899b8df9ab2f2841f
admodev <admosdigital@yahoo.com>
2021-04-19 07:06:54 -0300

    init project

---
+++ src/pages/Onboarding.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Container, Nav } from "react-bootstrap";
+import "../styles/onboarding.css";
+
+const Onboarding = () => {
+  return (
+    <Container className="innerContainer">
+      <Nav
+        activeKey="/Onboarding"
+        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+      >
+        <Nav.Item>
+          <Nav.Link href="/Onboarding" className="navLink">
+            Adolfo Moyano
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1" className="navLink">
+            Acerca de Mí
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2" className="navLink">
+            Mi Trabajo
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-3" className="navLink">
+            Mis Emprendimientos
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </Container>
+  );
+};
+
+export default Onboarding;
